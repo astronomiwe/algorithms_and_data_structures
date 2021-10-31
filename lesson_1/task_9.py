963cdb119186940bc5a7e4a180f2cfb95e1d4a87
Nikita Maslov <masastro91@gmail.com>
2021-10-31 15:07:32 +0300

    task_9 comments

--- lesson_1/task_9.py
+++ lesson_1/task_9.py
@@ -1,10 +1,13 @@
 # Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого)
 
+
+# блок ввода чисел
 print('Введите три разных числа')
 first = int(input('первое: '))
 second = int(input('второе: '))
 third = int(input('третье: '))
 
+# блок сравнения и вывода
 if (second < first < third) or (third < first < second):
     print(first)
 else:
